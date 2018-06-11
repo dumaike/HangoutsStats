f5e89aca113e601447d247585bc83ded0464aeec
dumaike <mdwyer84@gmail.com>
2018-06-11 16:46:21 -0400

    Some json structure

---
+++ HangoutsStatsUnity/Assets/Scripts/JSON/ConversationsEntry.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationsEntry
+{
+	public Conversation conversation;
+	public List<Event> events;
+}
---
+++ HangoutsStatsUnity/Assets/Scripts/PrototypeParse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeParse : MonoBehaviour
+{
+	public TextAsset hangoutsText;
+
+	private string selfId;
+
+	public void Start()
+	{
+
+		Debug.Log(hangoutsText.text);
+
+		Conversations allConvos = JsonConvert.DeserializeObject<Conversations>(hangoutsText.text);
+
+
+
+		foreach (ConversationsEntry entry in allConvos.conversations)
+		{
+			foreach (ParticipantData user in entry.conversation.conversation.participant_data)
+			{
+				Debug.Log(user.fallback_name);
+			}
+		}
+	}
+}
